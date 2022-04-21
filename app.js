1ab275e4b0b37a32746f7a4866d6262547ceed05
Pascoal98 <brunompp98@gmail.com>
2022-04-21 19:16:46 +0100

    Playing is possible now Player vs Player

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var loa_1 = null;
 var player1 = 'random';
 var player2 = 'random';
+var nTracker = 0;
+
 function newgame() {
   player1 = document.getElementById("p1ai").value;
   player2 = document.getElementById("p2ai").value;
@@ -64,30 +66,88 @@ function clearboard() {
 }
 
 function myclick(n) {
-  console.log('a', n);
-  clearPossibleMoves();
-  //console.log(document.getElementsByClassName("grid-item")[n]);
+  //console.log('a', n);
+  //console.log(nTracker);
+
   if (!loa_1 || loa_1.existe_win) return;
   let board = loa_1.board;
 
   let pieceX = Math.floor(n/8);
   let pieceY = Math.floor(n%8);
-  console.log(pieceX);
-  console.log(pieceY);
+  //console.log(pieceX + ' ' + pieceY);
   let piece = board[pieceX][pieceY];
-  console.log(piece);
-  if(piece == 'B') {
-    let possibleMoves = loa.get_piece_moves({'board':board, turn: 'B'}, {'x':pieceX, 'y':pieceY}, 'B');
-    console.log(possibleMoves);
-    for(let i = 0; i < possibleMoves.length; i++) {
-      let x = possibleMoves[i][0];
-      let y = possibleMoves[i][1];
-      console.log(possibleMoves[i][0] + ' ' + possibleMoves[i][1]);
-      let pos = x*8 + y;
-      console.log(pos);
-      if(document.getElementById(pos).className == "") {
-        document.getElementById(pos).className = "piece possibleBlack";
+  //console.log(piece);
+  
+  if(loa_1.turn == 'B') {
+
+    if(piece == 'B') {
+      clearPossibleMoves();
+      draw();
+      info();
+      nTracker = n;
+      console.log(nTracker + ' ntracker');
+      let possibleMoves = loa.get_piece_moves({'board':board, turn: 'B'}, {'x':pieceX, 'y':pieceY}, 'B');
+      //console.log(possibleMoves);
+      for(let i = 0; i < possibleMoves.length; i++) {
+        let x = possibleMoves[i][0];
+        let y = possibleMoves[i][1];
+        //console.log(possibleMoves[i][0] + ' ' + possibleMoves[i][1]);
+        let pos = x*8 + y;
+        //console.log(pos);
+        if(document.getElementById(pos).className != "piece pieceblack") {
+          document.getElementById(pos).className = "piece possibleBlack";
+        }
       }
+    } else 
+    
+    if(document.getElementById(n).className == "piece possibleBlack") {
+
+      let oldX = Math.floor(nTracker/8);
+      let oldY = Math.floor(nTracker%8);
+      console.log(oldX + ' ' + oldY + ' ' + nTracker);
+      loa_1.board[oldX][oldY] = '.';
+      loa_1.board[pieceX][pieceY] = 'B';
+
+      clearPossibleMoves();
+      loa_1.turn = 'W';
+      draw();
+      info();
+    }
+  } else {
+  
+    if(piece == 'W') {
+      clearPossibleMoves();
+      draw();
+      info();
+      nTracker = n;
+      console.log(nTracker + ' ntracker');
+      let possibleMoves = loa.get_piece_moves({'board':board, turn: 'W'}, {'x':pieceX, 'y':pieceY}, 'W');
+      console.log(possibleMoves);
+      for(let i = 0; i < possibleMoves.length; i++) {
+        let x = possibleMoves[i][0];
+        let y = possibleMoves[i][1];
+        console.log(possibleMoves[i][0] + ' ' + possibleMoves[i][1]);
+        let pos = x*8 + y;
+        console.log(pos);
+        if(document.getElementById(pos).className != "piece piecewhite") {
+          document.getElementById(pos).className = "piece possibleWhite";
+        }
+      }
+    } 
+    else 
+    
+    if(document.getElementById(n).className == "piece possibleWhite") {
+
+      let oldX = Math.floor(nTracker/8);
+      let oldY = Math.floor(nTracker%8);
+      console.log(oldX + ' ' + oldY + ' ' + nTracker);
+      loa_1.board[oldX][oldY] = '.';
+      loa_1.board[pieceX][pieceY] = 'W';
+
+      clearPossibleMoves();
+      loa_1.turn = 'B';
+      draw();
+      info();
     }
   }
 }
