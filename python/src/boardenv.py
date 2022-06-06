72d8b51298a80ef53a674aa40577fb03cc7a1799
Gabriel <gooloso98@gmail.com>
2022-06-06 23:40:20 +0100

    random start

--- python/src/boardenv.py
+++ python/src/boardenv.py
@@ -2,12 +2,23 @@ import gym
 from gym import spaces
 import numpy as np
 from jogo import Loa
+from random import randint as rand
+
+def randBoard(size, turn, changes):
+    board = np.array([Loa.E for _ in range(size*size)], dtype = int)
+    for _ in range(changes):
+        pos = rand(1, size*size) - 1
+        while board[pos] != Loa.E:
+            pos = rand(1, size*size) - 1
+        board[pos] = turn
+    return board
 
 class BoardEnv(gym.Env):
-    board = [ Loa.E, Loa.E, Loa.E, Loa.E,
-            Loa.B, Loa.E, Loa.E, Loa.B,
-            Loa.B, Loa.E, Loa.E, Loa.B,
-            Loa.E, Loa.E, Loa.E, Loa.E ]
+    board = [ Loa.E, Loa.B, Loa.E, Loa.E, Loa.E,
+            Loa.B, Loa.E, Loa.E, Loa.E, Loa.B,
+            Loa.E, Loa.E, Loa.E, Loa.E, Loa.B,
+            Loa.B, Loa.E, Loa.E, Loa.E, Loa.B,
+            Loa.E, Loa.E, Loa.B, Loa.E, Loa.E ]
     board = np.array(board, dtype = int)
             
     options = {
@@ -19,11 +30,15 @@ class BoardEnv(gym.Env):
     def __init__(self):
         self.size = BoardEnv.options["size"]
         super(BoardEnv, self).__init__()
-        self.action_space = spaces.Discrete(self.size * 4)
+        # self.changes = rand( int(.1*self.size*self.size), int(.6*self.size*self.size) )
+        self.changes = int(.2*self.size*self.size)
+        self.action_space = spaces.Discrete(self.changes * 4)
         self.observation_space = spaces.Box(low = -1, high = 1, shape = (self.size * self.size,), dtype = np.int64)
         # self.Loa = Loa(options = BoardEnv.options)
 
     def reset(self):
+        options = BoardEnv.options
+        options['board'] = randBoard(self.size, options['turn'], self.changes)
         self.Loa = Loa(options = BoardEnv.options)
         return self.Loa.board
 
@@ -32,7 +47,7 @@ class BoardEnv(gym.Env):
         piece = action // 4 # 4 possible moves
         moves = ["up", "right", "down", "left"]
         move = moves[action % 4]
-        print(self.Loa)
+        # print(self.Loa)
 
         self.Loa = self.Loa.play_relative(piece, move)
         win = self.Loa.status[0] != 0 # TODO : verify status[1]
