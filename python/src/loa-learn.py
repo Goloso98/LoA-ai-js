a1a854f666e0b408b22e2fbab625ab77adced9d9
ShirleyRodrigues <shirleyrodrigues2209@gmail.com>
2022-06-06 14:55:24 +0100

    Trainned new models

--- python/src/loa-learn.py
+++ python/src/loa-learn.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import DQN
 import os
 from boardenv import BoardEnv
 import time
@@ -17,12 +17,12 @@ if not os.path.exists(logdir):
 env = BoardEnv()
 env.reset()
 
-model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
+model = DQN('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
 
 TIMESTEPS = 10000
 iters = 0
 
 for i in range(30):
 	iters += 1
-	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO")
+	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"DQN")
 	model.save(f"{models_dir}/{TIMESTEPS*iters}")
